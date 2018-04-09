90e61360b9de1761e2cf52417801f9ed62065eaf
grichner <grichner@minastirith.fritz.box>
2018-04-09 12:46:47 +0200

    source added

--- bin/PROXY.sh
+++ bin/PROXY.sh
@@ -5,3 +5,5 @@ export ETH1_IP=` /bin/ifconfig enp2s0   | grep 'inet ' | awk '{ print $2}'`
 echo 'export HTTP_PROXY=http://dns:3128
 export HTTPS_PROXY=https://dns:3128'>/etc/profile.d/PROXY.sh
 echo 'export NO_PROXY='`echo ${ETH0_IP}`','`echo ${ETH1_IP}`','`echo ${WLAN0_IP}`',127.0.0.1,localhost'>>/etc/profile.d/PROXY.sh
+
+source /etc/profile.d/PROXY.sh
