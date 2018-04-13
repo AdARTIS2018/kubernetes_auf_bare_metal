de40cf621c51e358336f70c684806717d2d9c73c
grichner <grichner@minastirith.fritz.box>
2018-04-13 14:10:38 +0200

    update-check added

--- minikube/opensuse-fabric8.sh
+++ minikube/opensuse-fabric8.sh
@@ -5,7 +5,7 @@
 ##curl -L  https://github.com/dhiltgen/docker-machine-kvm/ -o /usr/local/bin/docker-machine-driver-kvm
 ##chmod +x /usr/local/bin/docker-machine-driver-kvm
 
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.25.0/minikube-linux-amd64
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.26.0/minikube-linux-amd64
 chmod +x minikube 
 mv minikube /usr/local/bin/
 rm -Rf ~/.minikube
@@ -17,6 +17,8 @@ systemctl enable libvirtd.service;systemctl start libvirtd.service
 virsh net-start default
 minikube start --vm-driver=kvm --cpus 2 --memory 14000 --disk-size 70g
 eval $(minikube docker-env)
+minikube update-check
+
 wget https://github.com/fabric8io/gofabric8/releases/download/v0.4.176/gofabric8-linux-amd64
 chmod a+x gofabric8-linux-amd64
 export GITHUB_OAUTH_CLIENT_ID=6fbfee4e07e179c5eb66
