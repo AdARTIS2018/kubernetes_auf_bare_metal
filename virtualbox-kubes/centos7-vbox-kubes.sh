4d7eb3308eccb468d53ac6f711f99ede68d7fbc1
grichner <grichner@minastirith.fritz.box>
2018-04-19 11:36:05 +0200

    golang installiert build cri-tools path set

--- virtualbox-kubes/centos7-vbox-kubes.sh
+++ virtualbox-kubes/centos7-vbox-kubes.sh
@@ -3,7 +3,7 @@ echo make sure swap is off swapoff -a OR don't install a swap partition at all!!
 echo ================================
 
 yum -y --enablerepo=extras install epel-release
-yum -y install dkms net-tools mlocate wget docker acpid
+yum -y install dkms net-tools mlocate wget docker acpid jq
 yum -y groupinstall "Development Tools"
 yum -y install kernel-devel
 ## vorher das Image auf dem HOST mounten _(VBoxGuestAdditions.iso)_
@@ -26,8 +26,17 @@ repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
 setenforce 0
-yum install -y kubelet kubeadm kubectl
+yum install -y kubelet kubeadm kubectl golang
 systemctl enable kubelet && systemctl start kubelet
+
+mkdir download
+cd download
+wget https://github.com/kubernetes-incubator/cri-tools/archive/master.zip
+unzip master.zip
+cd cri-tools-master
+make install
+export PATH=$PATH:/usr/local/bin
+echo "export=PATH=$PATH:/usr/local/bin" > /etc/profile.d/myPATH.sh
 
 ##centos routing problems
 cat <<EOF >  /etc/sysctl.d/k8s.conf
@@ -36,7 +45,9 @@ net.bridge.bridge-nf-call-iptables = 1
 EOF
 sysctl --system
 
-##Configure SELINUX=disabled OR permissive in the /etc/selinux/config file
+echo Configure SELINUX=disabled OR permissive in the /etc/selinux/config file
+echo ========================================================================
+
 docker info | grep -i cgroup
 cat /etc/systemd/system/kubelet.service.d/10-kubeadm.conf |grep cgroup-driver
 echo the above lines must be equal!!!! or kubelet won't start
